8da72e2309005c3404592c0c03ac83b351188710
Zhiyuan Wang <motif-linux@live.com>
2016-05-12 21:40:17 +0800

    Get SysInfo Page/API

--- private/src/AzureSatori/SatoriPortal/routes/index.ts
+++ private/src/AzureSatori/SatoriPortal/routes/index.ts
@@ -1,5 +1,10 @@
 /// <reference path="../typings/main.d.ts" />
+/// <reference path="../common/sysinfo.ts" />
+
 import Express = require('express');
+
+import Common = require("../common/sysinfo");
+
 var router = Express.Router();
 
 /* GET home page. */
@@ -7,5 +12,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/sysinfo", function (req, res, next) {
+    Common.sysinfo(
+        (e, d) => {
+            if (e != null) {
+                res.statusMessage = "Cannot get system info: " + JSON.stringify(e);
+                res.send(500);
+
+            } else {
+                var json = JSON.stringify(d, null, 4);
+                res.setHeader("Content-Type", "application/json");
+                res.send(json);
+            }
+        }
+    );
+});
 
 module.exports = router;
