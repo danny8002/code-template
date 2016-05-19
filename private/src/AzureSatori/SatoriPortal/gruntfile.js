fc37e8fd600d726f0dadddb4e5fc57e551437292
Zhiyuan Wang <motif-linux@live.com>
2016-05-19 11:36:22 +0800

    Move output to dist/ folder

--- private/src/AzureSatori/SatoriPortal/gruntfile.js
+++ private/src/AzureSatori/SatoriPortal/gruntfile.js
@@ -2,29 +2,40 @@
 
 var Fs = require('fs');
 
+function tsc2Grunt(tsConfig) {
+	var tscOptions = tsConfig.compilerOptions;
+
+	// convert outDir option to Grunt-ts style options
+	var gruntTSC = { options: tscOptions };
+	if (tscOptions.outDir != null) {
+		gruntTSC.files = {};
+		gruntTSC.files[tscOptions.outDir] = tsConfig.files;
+	} else {
+		gruntTSC.src = tsConfig.files;
+	}
+
+	return gruntTSC;
+}
+
 module.exports = function (grunt) {
-	
+
+	var outDir = "dist/";
+	grunt.log.debug("Output folder: " + outDir);
+
 	grunt.log.debug('Read tsconfig.json from current project directory!');
 	var tsConfig = grunt.file.readJSON('tsconfig.json');
-	
-	var cacheFile = '.gruntWatch.cache';
-	
-	
-	var folders = (tsConfig['exclude'] || []).map(function (i) { return '!' + i + '/**/*.ts'; });
-	folders.unshift('!**/*.d.ts');
-	folders.unshift('**/*.ts');
-	
-	grunt.log.debug('TypeScript compile folders:' + folders);
-	
+
+	if (tsConfig.compilerOptions.outDir) {
+		grunt.log.debug('[outDir] in tsconfig.json is ignored, use: ' + outDir);
+		tsConfig.compilerOptions.outDir = outDir;
+	}
+
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		ts: {
-			default: {
-				src: tsConfig['files'],
-				options: tsConfig['compilerOptions']
-			}
+			default: tsc2Grunt(tsConfig)
 		},
-		
+
 		less: {
 			dev: {
 				options: {
@@ -37,12 +48,31 @@ module.exports = function (grunt) {
 					compress: true
 				}
 			}
+		},
+
+		copy: {
+			default: {
+				files: [
+					{
+						expand: true,
+						src: [
+							"**",
+							"!node_modules/**",
+							"!" + outDir + "**"
+						],
+						dest: outDir,
+						filter: 'isFile'
+					}
+				]
+			}
 		}
 	});
-	
+
 	grunt.loadNpmTasks("grunt-ts");
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	
-	grunt.registerTask("default", ["ts", "less:prod"]);
+	grunt.loadNpmTasks('grunt-contrib-copy');
+
+	grunt.registerTask("default", ["ts:default", "less:dev", "copy:default"]);
+	grunt.registerTask("prod", ["ts:default", "less:prod", "copy:default"]);
 }
